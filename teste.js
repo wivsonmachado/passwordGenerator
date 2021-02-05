99e59afe1459dbd0dce80178363eeadf2bd4897d
Wivson Machado <wivsonfrota@gmail.com>
2021-02-05 12:23:09 -0300

    Inserção de variavel para escolhar quantidade de caracteres na senha porém somente letras maiúsculas

--- teste.js
+++ teste.js
@@ -1,19 +1,21 @@
-function password(){
+function password(tam = 8){
 
-    caract1 = (min, max) => {
+    caract1 = (min = 64, max = 90) => {
         let n1 = Math.floor(Math.random() * (max - min) + min)
         let n2 = String.fromCharCode(n1)
         return n2
     }
 
-    let pass =`${caract1(48,57)}${caract1(64,90)}${caract1(97,122)}${caract1(35,38)}${caract1(48,57)}${caract1(64,90)}${caract1(97,122)}${caract1(35,38)}`
-    return pass    
-}
+    let stringpass = ''
+    let x = 1
+    do{
+        stringpass = stringpass + `${caract1()}`
+        x++
+    }while(x <= tam)
+    console.log(stringpass)
 
-let i = 1
-let arry = []
-do{
-    arry.push(password())
-    i++    
-}while(i <= 10)
-console.log(arry)
+}
+password()
+password(10)
+password(15)
+password(20)
